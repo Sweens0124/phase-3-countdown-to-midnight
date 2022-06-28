f220c9c71d3047634cd2607736706829bbf8e28a
Lucas Sweeney <lsweeney0124@gmail.com>
2022-06-27 22:53:57 -0500

    commit tree 268ffd4a960142647dda235821d5f97418a34343

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
+require 'pry'
 #write your code here
 
-def countdown
+
+def countdown num
+  num = 10
+  while num > 0
+    puts "#{num} SECOND(S)"
+    num -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
+
